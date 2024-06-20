f5c257f51c263e94cd5044cac016689f58e35d71
Abraxas-365 <luisfmiranda8@gmail.com>
2024-06-20 15:34:19 -0500

    feat: add environment initialization functionality
    Add a new set of functions in initialize.go under cmd/env/initialize for reading application name from input and creating a new file '.hyphen-env-key' with app name and a generated secret key stored in toml format. The secret key is generated using crypto/rand package and encoded to base64 string in secretkey.go under internal/secretkey.

--- cmd/env/initialize/initialize.go
+++ cmd/env/initialize/initialize.go
@@ -1,9 +1,22 @@
 package initialize
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/Hyphen/cli/internal/secretkey"
 	"github.com/spf13/cobra"
+
+	"github.com/BurntSushi/toml"
 )
+
+type Config struct {
+	AppName   string `toml:"app_name"`
+	SecretKey string `toml:"secret_key"`
+}
 
 var InitCmd = &cobra.Command{
 	Use:     "init",
@@ -11,6 +24,44 @@ var InitCmd = &cobra.Command{
 	Short:   "Initialize the environment",
 	Long:    `This command initializes the environment with necessary configurations.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		appName, err := PromptForAppName(os.Stdin)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//TODO:
+		//check if name exist
+		//xxx
+
+		// Create and write to the .hyphen-env-key file
+		config := Config{
+			AppName:   appName,
+			SecretKey: secretkey.New().Base64(),
+		}
+
+		file, err := os.Create(".hyphen-env-key")
+		if err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer file.Close()
+
+		enc := toml.NewEncoder(file)
+		if err := enc.Encode(config); err != nil {
+			fmt.Println("Error encoding TOML:", err)
+			return
+		}
+
 		fmt.Println("Environment initialized")
 	},
+}
+
+func PromptForAppName(reader io.Reader) (string, error) {
+	r := bufio.NewReader(reader)
+	fmt.Print("Name of App: ")
+	appName, err := r.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("error reading input: %w", err)
+	}
+	return strings.TrimSpace(appName), nil
 }
---
+++ internal/secretkey/secretkey.go
@@ -0,0 +1,51 @@
+package secretkey
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"os"
+)
+
+type SecretKey struct {
+	secretBase64 string
+}
+
+func FromBase64(secretBase64 string) *SecretKey {
+	return &SecretKey{
+		secretBase64: secretBase64,
+	}
+}
+
+func New() *SecretKey {
+
+	secret := make([]byte, 256)
+	if _, err := rand.Read(secret); err != nil {
+		fmt.Println("Error generating secret key:", err)
+		os.Exit(1)
+	}
+
+	secretBase64 := base64.StdEncoding.EncodeToString(secret)
+	if len(secretBase64) > 256 {
+		secretBase64 = secretBase64[:256]
+	}
+
+	return &SecretKey{
+		secretBase64: secretBase64,
+	}
+}
+
+func (s SecretKey) Base64() string {
+	return s.secretBase64
+}
+
+func (s SecretKey) HashSHA() string {
+	hasher := sha256.New()
+	if _, err := hasher.Write([]byte(s.secretBase64)); err != nil {
+		fmt.Println("Error hashing secret key:", err)
+		os.Exit(1)
+	}
+
+	return fmt.Sprintf("%x", hasher.Sum(nil))
+}
