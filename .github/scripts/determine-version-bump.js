9601f8678ef010ca3063a988387eaf854bc23910
Abraxas-365 <luisfmiranda8@gmail.com>
2024-08-19 18:43:51 -0500

    feat:test

--- .github/scripts/determine-version-bump.js
+++ .github/scripts/determine-version-bump.js
@@ -1,47 +1,45 @@
-const core = require('@actions/core');
 const { execSync } = require('child_process');
 
-try {
-  const latestRelease = process.env.latest_release.replace(/^"(.*)"$/, '$1').replace(/\\"/g, '"');
-  const latestVersion = JSON.parse(latestRelease).version;
-  const bumpInfo = JSON.parse(latestRelease).bumpInfo;
+function determineVersionBump() {
+  // Get the latest release information
+  const latestRelease = JSON.parse(process.env.LATEST_RELEASE);
+  const latestVersion = latestRelease.version;
+  let bumpInfo = latestRelease.bumpInfo;
 
-  console.log("Latest version:", latestVersion);
-  console.log("Current bump info:", bumpInfo);
+  console.log(`Latest version: ${latestVersion}`);
+  console.log(`Current bump info: ${JSON.stringify(bumpInfo)}`);
+
+  // Get the last 3 commits
+  const commits = execSync('git log -n 3 --pretty=format:"%s%n%b"').toString();
 
   console.log("Analyzing the last 3 commits:");
-  const commits = execSync('git log -n 3 --pretty=format:"%s%n%b"').toString();
-  console.log("Commits to analyze:");
   console.log(commits);
   console.log("---End of commits---");
 
-  let bumpType;
-  if (/(\n|^)BREAKING CHANGE:/.test(commits) || /^[^:]+!/.test(commits)) {
-    bumpType = "major";
+  let bumpType = 'patch';
+
+  if (/(\n|^)BREAKING CHANGE:/.test(commits) || /^[^:]+!:/.test(commits)) {
+    bumpType = 'major';
   } else if (/^feat(\(.+\))?:/.test(commits)) {
-    console.log("FEAT")
-    bumpType = "minor";
-  } else {
-    bumpType = "patch";
+    bumpType = 'minor';
   }
 
-  const newBumpInfo = JSON.parse(JSON.stringify(bumpInfo)); 
-  if (bumpType === "major") {
-    newBumpInfo.major = true;
-    newBumpInfo.minor = true;
-    newBumpInfo.patch = true;
-  } else if (bumpType === "minor") {
-    newBumpInfo.minor = true;
-    newBumpInfo.patch = true;
+  // Update bump info
+  if (bumpType === 'major') {
+    bumpInfo = { major: true, minor: true, patch: true };
+  } else if (bumpType === 'minor') {
+    bumpInfo = { ...bumpInfo, minor: true, patch: true };
   } else {
-    newBumpInfo.patch = true;
+    bumpInfo = { ...bumpInfo, patch: true };
   }
 
-  // Output the results
-  console.log("Determined bump type:", bumpType);
-  core.setOutput('bump_type', bumpType);
-  core.setOutput('new_bump_info', JSON.stringify(newBumpInfo));
+  console.log(`Determined bump type: ${bumpType}`);
+  console.log(`New bump info: ${JSON.stringify(bumpInfo)}`);
 
-} catch (error) {
-  core.setFailed(error.message);
+  return { bumpType, bumpInfo };
 }
+
+// Run the function and output results
+const result = determineVersionBump();
+console.log(`::set-output name=bump_type::${result.bumpType}`);
+console.log(`::set-output name=new_bump_info::${JSON.stringify(result.bumpInfo)}`);
