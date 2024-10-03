6ed5f8c9366a23a49bf94d65cfaee0f9b653d939
Kyle Kamperschroer <kyle@kampy.dev>
2024-10-03 10:51:26 -0500

    Remove secret for client (use PKCE correctly), and support both prod and dev environments

--- pkg/apiconf/conf.go
+++ pkg/apiconf/conf.go
@@ -1,19 +1,29 @@
 package apiconf
 
-import "os"
+import (
+	"os"
+	"strings"
+
+	"github.com/Hyphen/cli/pkg/flags"
+)
 
 func GetBaseApixUrl() string {
-	baseUrl := os.Getenv("HYPHEN_CUSTOM_APIX")
-	if baseUrl == "" {
-		baseUrl = "https://api.hyphen.ai"
+	if flags.DevFlag || strings.ToLower(os.Getenv("HYPHEN_DEV")) == "true" {
+		return "https://dev-api.hyphen.ai"
 	}
-	return baseUrl
+	return "https://api.hyphen.ai"
 }
 
 func GetBaseAuthUrl() string {
-	baseUrl := os.Getenv("HYPHEN_CUSTOM_AUTH")
-	if baseUrl == "" {
-		baseUrl = "https://auth.hyphen.ai"
+	if flags.DevFlag || strings.ToLower(os.Getenv("HYPHEN_DEV")) == "true" {
+		return "https://dev-auth.hyphen.ai"
 	}
-	return baseUrl
+	return "https://auth.hyphen.ai"
+}
+
+func GetAuthClientID() string {
+	if flags.DevFlag || strings.ToLower(os.Getenv("HYPHEN_DEV")) == "true" {
+		return "8d5fb36d-2886-4c53-ab70-e6203e781fbc"
+	}
+	return "e6315ab1-5847-4c75-a003-65b5ed374dd1"
 }
