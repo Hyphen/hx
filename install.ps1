27ee19c31bf0d51a46bd1cf4e0f06059971b6181
Abraxas-365 <luisfmiranda8@gmail.com>
2024-10-09 13:54:25 -0500

    chore: test

--- install.ps1
+++ install.ps1
@@ -61,7 +61,7 @@ function Install-CLI {
         [string]$version
     )
     $packageName = "hyphen-cli"
-    $os = "windows" 
+    $os = "windows"  # Hardcoded to windows
 
     if (-not $version) {
         $version = Get-LatestVersion -packageName $packageName
