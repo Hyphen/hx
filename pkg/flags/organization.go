93d511543be13e1ec1e3e7ee82fb93b8efb8155a
Abraxas-365 <luisfmiranda8@gmail.com>
2024-11-08 10:23:15 -0500

    refactor: moving as how was main, so the pr will be easy to understand

--- pkg/flags/organization.go
+++ pkg/flags/organization.go
@@ -2,7 +2,6 @@ package flags
 
 import (
 	"github.com/Hyphen/cli/internal/manifest"
-	"github.com/Hyphen/cli/pkg/cprint"
 	"github.com/Hyphen/cli/pkg/errors"
 )
 
@@ -11,33 +10,14 @@ func GetOrganizationID() (string, error) {
 		return OrganizationFlag, nil
 	}
 
-	m, err := manifest.RestoreConfig()
+	manifest, err := manifest.RestoreConfig()
 	if err != nil {
 		return "", err
 	}
 
-	if m.OrganizationId == "" {
+	if manifest.OrganizationId == "" {
 		return "", errors.New("No organization ID provided and no default found in manifest")
 	}
 
-	if !isGlobalOrgIdSameAsLocal() {
-		cprint.Warning("The app organization ID is different from the global organization ID. This could lead to unexpected behavior.", VerboseFlag)
-	}
-
-	return m.OrganizationId, nil
-}
-
-func isGlobalOrgIdSameAsLocal() bool {
-	ml, _ := manifest.RestoreLocalConfig() // we can skip the error, if its new project will no have ml
-	if ml.OrganizationId == "" {
-		return true //if we dont have ml return true bc is new project
-	}
-
-	mg, _ := manifest.RestoreGlobalConfig() // we can skip this error check, bc whe check the error in the line  16
-
-	if ml.OrganizationId != mg.OrganizationId {
-		return false
-	}
-
-	return true
+	return manifest.OrganizationId, nil
 }
