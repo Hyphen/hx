b78c1bad5619f9f3ac7165e6fe284e459d42e956
Brad Wilson <dotnetguy@gmail.com>
2025-10-14 10:04:56 -0700

    Set up ~/.hx manually with values from environment variables (#213)

--- cmd/initapp/hyphen-entrypoint.sh
+++ cmd/initapp/hyphen-entrypoint.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
-if ! command -v "wget" >/dev/null; then
-    echo "Missing command 'wget'. Please install wget into the Docker container." 1>&2
-    exit 1
-fi
+verify_command() {
+    if ! command -v $1 >/dev/null; then
+        echo "Missing command '$1'. Please install $1 into the Docker container." 1>&2
+        exit 1
+    fi
+}
 
-if ! command -v "sed" >/dev/null; then
-    echo "Missing command 'sed'. Please install sed into the Docker container." 1>&2
-    exit 1
-fi
+verify_command "wget"
+verify_command "sed"
+
+verify_environment() {
+    if ! printenv $1 >/dev/null; then
+        echo "Missing environment variable: $1" 1>&2
+        exit 1
+    fi
+}
+
+verify_environment "HYPHEN_API_KEY"
+verify_environment "HYPHEN_APP_ENVIRONMENT"
+verify_environment "HYPHEN_APP_ID"
+verify_environment "HYPHEN_PROJECT_ID"
+verify_environment "HYPHEN_ORGANIZATION_ID"
 
 if [ $# -eq 0 ]; then
     echo "Missing execution command line. Did you forget a CMD in your Dockerfile?" 1>&2
-    exit 1
-fi
-
-if [ -z "${HYPHEN_API_KEY}" ]; then
-    echo "Missing environment variable: HYPHEN_API_KEY" 1>&2
-    exit 1
-fi
-
-if [ -z "${HYPHEN_APP_ENVIRONMENT}" ]; then
-    echo "Missing environment variable: HYPHEN_APP_ENVIRONMENT" 1>&2
     exit 1
 fi
 
@@ -50,12 +53,14 @@ if ! [ -f ".hyphen/hx" ]; then
     chmod +x ".hyphen/hx"
 fi
 
-echo ">>> Logging into Hyphen..."
+echo ">>> Creating ~/.hx ..."
 
-./.hyphen/hx auth --use-api-key
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+echo "{"                                                      > ~/.hx
+echo "  \"hyphen_api_key\": \"${HYPHEN_API_KEY}\"",          >> ~/.hx
+echo "  \"organization_id\": \"${HYPHEN_ORGANIZATION_ID}\"", >> ~/.hx
+echo "  \"project_id\": \"${HYPHEN_PROJECT_ID}\"",           >> ~/.hx
+echo "  \"app_id\": \"${HYPHEN_APP_ID}\""                    >> ~/.hx
+echo "}"                                                     >> ~/.hx
 
 echo ">>> Pulling environment variables..."
 
